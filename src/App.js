9eb857ae0d9b52bc081c2feecda3f5552c89453f
ddgpmc <111261238+ddgpmc@users.noreply.github.com>
2024-09-12 17:09:02 +0800

    package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import FAQs from './components/FAQs';
